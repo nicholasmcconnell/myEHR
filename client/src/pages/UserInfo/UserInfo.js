1fb7e8fe734ee4c2095bbbcdb23b5e8a435129eb
Alicia Burn <Aliciaburn@gmail.com>
2020-04-23 14:41:45 -0400

    Worked on Medications page and UserInfo page (#6)

--- client/src/pages/UserInfo/UserInfo.js
+++ client/src/pages/UserInfo/UserInfo.js
@@ -7,7 +7,45 @@ export default function UserInfo() {
 
     return (
         <Container>
-            
+            <form>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                    <label for="">Name</label>
+                    <input type="" class="form-control" placeholder=""/>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="inputAddress">Birthdate</label>
+                    <input type="text" class="form-control"  placeholder="mm/dd/yyyy"/>
+                </div>
+                <div class="form-group">
+                    <label for="">Blood Type</label>
+                    <select id="" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>A-positive</option>
+                        <option>A-negative</option>
+                        <option>B-positive</option>
+                        <option>B-negative</option>
+                        <option>AB-positive</option>
+                        <option>AB-negative</option>
+                        <option>O-positive</option>
+                        <option>O-negative</option>
+                    </select>
+                </div>
+                    <div class="form-group">
+                    <label for="">Allergies</label>
+                    <textarea type="text" class="form-control" id=""/>
+                    </div>
+                    <div class="form-group">
+                    <label for="">Diagnoses</label>
+                    <textarea type="text" class="form-control" id=""/>
+                    </div>
+                    <div class="form-group">
+                    <label for="">Family History</label>
+                    <textarea type="text" class="form-control" id=""/>
+                    </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </Container>
     )
 }
