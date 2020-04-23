1a7fb58fc3902b8311878d2cb89bdea4af0dd1e1
Alicia Burn <Aliciaburn@gmail.com>
2020-04-23 12:38:04 -0400

    Page setup (#5)
    * landing page
    
    * initial set up of Profile.js and SignUp.js

---
+++ client/src/pages/UserInfo/UserInfo.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from '../../components/Grid';
+import { Button, Input } from '../../components/Forms';
+
+export default function UserInfo() {
+
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
