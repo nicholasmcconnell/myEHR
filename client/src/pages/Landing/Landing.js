c8854cb8044a1730dde2063f600c8824544ae237
Alicia Burn <Aliciaburn@gmail.com>
2020-04-24 10:32:10 -0400

    Landing page skeleton (#9)

---
+++ client/src/pages/Landing/Landing.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from '../../components/Grid';
+import { Button, Input } from '../../components/Forms';
+
+export default function Landing() {
+
+    return (
+        <Container>
+            <div class="jumbotron">
+                <h1 class="display-4">My EHR</h1>
+                <p class="lead">All your medical records in one place.</p>
+                <hr class="my-4"/>
+                <p></p>
+                <a class="btn btn-primary btn-lg" href="../SignUp" role="button">Get Started</a>
+            </div>
+        </Container>
+    )
+}
+
