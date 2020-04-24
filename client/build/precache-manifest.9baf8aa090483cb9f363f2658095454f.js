c1ca893dc140ad83ea71b1f6d75c9e45416ffcf8
Nick McConnell <nicholas.mcconnell@gmail.com>
2020-04-23 20:59:16 -0400

    mock axios returns made, test database calls implimented (#8)

---
+++ client/build/precache-manifest.9baf8aa090483cb9f363f2658095454f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b9027b842f7d8407046",
+    "url": "/static/css/main.a44c03d3.chunk.css"
+  },
+  {
+    "revision": "6b9027b842f7d8407046",
+    "url": "/static/js/main.c2457305.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "33331ac05f842e2a22de",
+    "url": "/static/js/2.9fe96f8a.chunk.js"
+  },
+  {
+    "revision": "dfab9e191ff03a7591c93d11c986ff98",
+    "url": "/static/media/Logo.dfab9e19.png"
+  },
+  {
+    "revision": "ef60f99edcade593d5c8dd4c14cdd809",
+    "url": "/index.html"
+  }
+];
