9a3b843a7e5e10f048e67045de3726116f6c5b04
Nick McConnell <nicholas.mcconnell@gmail.com>
2020-04-26 23:47:23 -0400

    Heroku push (#13)
    * run build was executed
    
    * routes edits
    
    * test
    
    * uncommented path
    
    * passport new user username post route complete

---
+++ scripts copy/seedDB.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/googlebooks"
+);
+
+const bookSeed = [
+  {
+    title: "The Dead Zone",
+    author: "Stephen King",
+    synopsis:
+      "A number-one national best seller about a man who wakes up from a five-year coma able to see people's futures and the terrible fate awaiting mankind in The Dead Zone - a \"compulsive page-turner\" (The Atlanta Journal-Constitution). Johnny Smith awakens from a five-year coma after his car accident and discovers that he can see people's futures and pasts when he touches them. Many consider his talent a gift; Johnny feels cursed. His fiancée married another man during his coma, and people clamor for him to solve their problems. When Johnny has a disturbing vision after he shakes the hand of an ambitious and amoral politician, he must decide if he should take drastic action to change the future. The Dead Zone is a \"faultlessly paced...continuously engrossing\" (Los Angeles Times) novel of second sight.",
+    image: "pic href"
+  },
+  {
+    title: "where the wild things are",
+    author: "richard scary?",
+    synopsis:
+      "WILD WILD WILD THINGS RAWR",
+    image: "WILD PIC"
+  },
+  {
+    title: "Get a spoon",
+    author: "nick",
+    synopsis:
+      "a story about sharing",
+    image: "spoon PIC"
+  }
+]
+ 
+
+db.Book
+  .remove({})
+  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
