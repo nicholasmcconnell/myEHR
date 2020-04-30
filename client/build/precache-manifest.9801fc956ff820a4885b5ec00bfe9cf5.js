8e9699248eddd8a2bd643cedd930814e71704903
drthisguy <page.c.tyler@gmail.com>
2020-04-30 16:51:15 -0400

    merged and built wit nick's PR.

---
+++ client/build/precache-manifest.9801fc956ff820a4885b5ec00bfe9cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfab9e191ff03a7591c93d11c986ff98",
+    "url": "/static/media/Logo.dfab9e19.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08df34bb1c5b8df58c2b",
+    "url": "/static/js/main.63fcc1d7.chunk.js"
+  },
+  {
+    "revision": "40b33cd4e858800feb6f",
+    "url": "/static/js/2.8ce3f4a5.chunk.js"
+  },
+  {
+    "revision": "08df34bb1c5b8df58c2b",
+    "url": "/static/css/main.b6a3cca8.chunk.css"
+  },
+  {
+    "revision": "209d12c6cf72254345083bc897a5f305",
+    "url": "/index.html"
+  }
+];
