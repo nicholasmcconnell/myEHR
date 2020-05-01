c0033892fcd876d4e4f16545c364769b891b45da
dc <chendana95@gmail.com>
2020-05-01 06:26:29 -0400

    fixed route for register and login, got fetchProfile route working to print all profiles to console

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,55 +3,55 @@ require('dotenv').config();
 const apiKey = process.env.REACT_APP_API_KEY;
 
 export default {
-    //this should list all drugs 
-    getDrugs: function () {
+    getDrugs: function() {
         return axios.get("/api/drugs");
     },
     //this will be executed when the user selects "get more info" btn
-    getDrugInfo: function () {
+    getDrugInfo: function() {
         return axios.get(`https://www.dictionaryapi.com/api/v3/references/medical/json/cetirizine?key=${apiKey}`);
     },
 
-    getCondition: function ({ conditionSearch }) {
+    getCondition: function({ conditionSearch }) {
         return axios.get(`https://clinicaltables.nlm.nih.gov/api/conditions/v3/search?terms=${conditionSearch}&sf=primary_name,consumer_name&df=primary_name,consumer_name,info_link_data`)
-        .catch( err => console.log(err))
+            .catch(err => console.log(err))
     },
-     
-    updateEHR: function (id, data) {
-        
+
+    updateEHR: function(id, data) {
+
         return new Promise((resolve, reject) => {
             resolve({ status: 'success' })
         });
         //return axios.post(`/api/:${id}`, data)
     },
-    fetchUser: function () {
-        return {email: 'example@example.com', _id:'123456789'}
+    fetchUser: function() {
+        return { email: 'example@example.com', _id: '123456789' }
     },
 
-    fetchAccount: function () {
-        return {
-            id: "67",
-            firstName: "Eddie",
-            lastName: "Bravo",
-            birthday: "6/11/1983",
-            allergies: "peanuts. horseradish, bees",
-            medications: [{
-                name: "tylonal",
-                dosage: "the right amount",
-                date: "hey I got new meds today they are groovy - Pull new date"
-            }]
-        }
+    fetchProfile: function() {
+        return axios.get("/load");
+        // return {
+        //     id: "67",
+        //     firstName: "Eddie",
+        //     lastName: "Bravo",
+        //     birthday: "6/11/1983",
+        //     allergies: "peanuts. horseradish, bees",
+        //     medications: [{
+        //         name: "tylonal",
+        //         dosage: "the right amount",
+        //         date: "hey I got new meds today they are groovy - Pull new date"
+        //     }]
+        // }
     },
 
     register: function(credentials) {
-        console.log("in API.register");
-        console.log(credentials);
+        // console.log("in API.register");
+        // console.log(credentials);
         const { email, password } = credentials
-        return axios.post('/api/authenticate/register', { email, password })
+        return axios.post('/register', { email, password })
     },
 
     login: function(credentials) {
         const { email, password } = credentials;
-        return axios.post('/api/authenticate/login', { email, password })
+        return axios.post('/login', { email, password })
     }
 };
