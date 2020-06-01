4e1efb8b81c4bbd0f90fc206ec9f841452665a34
drthisguy <page.c.tyler@gmail.com>
2020-06-01 15:39:38 -0400

    resolved issue where signup page could not redirect to patients and so users were loggin in after sign up.

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,97 +1,104 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { Container, Row, Col } from '../../components/Grid';
-import { LoggerBtn, Input } from '../../components/Forms';
-
-import API from '../../utils/API';
-import Auth from '../../Auth'
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Container, Row, Col } from "../../components/Grid";
+import { LoggerBtn, Input } from "../../components/Forms";
+import API from "../../utils/API";
+import Auth from "../../Auth";
 
 export default function SignUp() {
 
-   
-    const [user, setUser] = useState(''),
-    [credentials, setCredentials] = useState({});
+    let history = useHistory();
+    const [credentials, setCredentials] = useState({}),
 
-     let history = useHistory();
-     
-    const onInputChange = e => {
-
+    onInputChange = e => {
         const { name, value } = e.target;
-        setCredentials({...credentials, [name]: value })
+        setCredentials({ ...credentials, [name]: value });
     },
 
     authorize = () => {
         Auth.login(() => {
-            history.push("/profiles")
-        })
-        }
+            history.push("/profiles");
+        });
+    },
 
-        const handleSubmit = e => {
+    handleSubmit = e => {
         e.preventDefault();
         e.target.reset();
 
-        API.register(credentials)
-        .then(({ data }) => { 
-    
-            if(data) {
-                console.log(data.email)
-            setUser({  email: data }); 
-            authorize()
-
-            } else {
-            console.log('response'+ data)
-            }
-        }).catch((err) => err)
-        }
-
+        try {
+            API.register(credentials)
+                .then(({ statusText }) => {
+                    if (statusText === 'OK') {
+                        API.login(credentials)
+                            .then(({ data }) => {
+                                if (data.status === 'success') {
+                                    authorize()
+                                }
+                            })
+                    }
+                }
+                )
+        } catch (err) { return }
+    }
     return (
-    
-        <Container classes={'box-shadow sign'}>
+        <Container classes={"box-shadow sign"}>
             <Row>
-                <Col size={'md-12'} >
+                <Col size={"md-12"}>
                     <Row>
-                        <div className={'mt-5'}>
+                        <div className={"mt-5"}>
                             <h5 style={text}>
-                                <img style={logo} src={require('../../assets/img/Logo.png')} alt={'Logo'} />
-                            Create your account
-                        </h5>
+                                <img
+                                    style={logo}
+                                    src={require("../../assets/img/Logo.png")}
+                                    alt={"Logo"}
+                                />
+                Create your account
+              </h5>
                         </div>
                     </Row>
-                    <Row >
-                        <form onSubmit={handleSubmit} className={'card-body'}>
-                            <div className={'form-group usr-inpt'}>
+                    <Row>
+                        <form onSubmit={handleSubmit} className={"card-body"}>
+                            <div className={"form-group usr-inpt"}>
                                 <label>Email:</label>
                                 <span className="fa fa-user" />
-                                <Input onChange={onInputChange}
-                                    name="email" type="email" placeholder="E-mail address" />
+                                <Input
+                                    onChange={onInputChange}
+                                    name="email"
+                                    type="email"
+                                    placeholder="E-mail address"
+                                />
                             </div>
-                            <div className={'form-group usr-inpt'}>
+                            <div className={"form-group usr-inpt"}>
                                 <label>Password:</label>
                                 <span className="fa fa-lock" />
-                                <Input onChange={onInputChange}
-                                    name="password" type="password" placeholder="Password" />
+                                <Input
+                                    onChange={onInputChange}
+                                    name="password"
+                                    type="password"
+                                    placeholder="Password"
+                                />
                             </div>
-                    
-                            <LoggerBtn btnType={'Sign Up'} />
 
+                            <LoggerBtn btnType={"Sign Up"} />
                         </form>
                     </Row>
-                    <Row classes={'justify-content-center'}>
-                        <p> Already have an account?
-                        <Link to="/SignIn" > Log In.</Link>
+                    <Row classes={"justify-content-center"}>
+                        <p>
+                            {" "}
+              Already have an account?
+              <Link to="/SignIn"> Log In.</Link>
                         </p>
                     </Row>
                 </Col>
             </Row>
         </Container>
-    )
+    );
 }
 const logo = {
     width: "60px",
     height: "auto"
 },
-
     text = {
         color: "#0099ff",
         textShadow: "1px 1px #000"
-    }
+    };
