563dfad69754d7230680530b79fb1eacf6cc855a
drthisguy <page.c.tyler@gmail.com>
2020-06-02 14:23:07 -0400

    confirm removal btn design completed.

--- client/src/components/PatientList/index.js
+++ client/src/components/PatientList/index.js
@@ -7,39 +7,35 @@ import { Button } from '../Forms';
 export function PatientList({ patient, context, removeState, confirmRemoval, index }) {
     if(!patient) return
 
-    let name,
-    { patientData } = patient,
-       { _id } = patient;
-
+    const { patientData } = patient,
+             { _id } = patient;
+    
+    let name;
     if (!patientData || (patientData.firstName === null && patientData.nickName === null)) {
         name = "name this patient"
     } else {
         name = patientData.nickname ? patientData.nickname : patientData.firstName;
     }
-    console.log(patient)
+
     if (removeState) {
         if(patient.removable) {
     return (
         <div className={'patient-remove'}>
         <Link to='/patients'
-            onClick={confirmRemoval.bind(this, index)}  //set patientContext as universal variable.  
+            onClick={confirmRemoval.bind(this, index)}  
             style={{textDecoration: 'none'}}
             >
-      
             <div className="card box-shadow mt-5">
                 <div className="card-body text-center btn btn-outline-danger">
                 <Row >
-                    <Col size ={"md-2"}></Col>
-                    <Col size ={"md-8"}> 
-                        <h2 className="card-title">Remove {name !== 'name this patient' ? name : 'this patient'}</h2>
-                        <p className="card-text"> 
-                            Delete {name !== 'name this patient' ? name : 'this patient'} from your patient list?
-                        </p>
+                <Col size={"md-4"}>
+                        <i className="fas fa-user-times fa-4x rm-lg-user" />   
                     </Col>
-                    <Col size={"md-2"}>
-                        <Button className="fas fa-user-times fa-2x" style={toggleBtn} 
-                            onClick={confirmRemoval.bind(this, index)} 
-                        />   
+                    <Col size ={"md-8"}> 
+                        <h2 className="card-title">Please Confirm</h2>
+                        <p className="card-text"> 
+                            This will remove {name !== 'name this patient' ? name : 'this patient'} from your list.
+                        </p>
                     </Col>
                 </Row>
                 </div>
@@ -51,7 +47,7 @@ export function PatientList({ patient, context, removeState, confirmRemoval, ind
     return (
         <div className={'patient-remove'}>
         <Link to='/patients'
-            onClick={confirmRemoval.bind(this, index)}  //set patientContext as universal variable.  
+            onClick={confirmRemoval.bind(this, index)}  
             style={{textDecoration: 'none'}}
             >
             <div className="card box-shadow mt-5">
@@ -65,7 +61,7 @@ export function PatientList({ patient, context, removeState, confirmRemoval, ind
                         </p>
                     </Col>
                     <Col size={"md-2"}>
-                        <Button className="fas fa-user-times fa-2x" style={toggleBtn} 
+                        <i className="fas fa-user-times fa-2x rm-sm-user" style={toggleBtn} 
                         />   
                     </Col>
                 </Row>
@@ -87,7 +83,7 @@ export function PatientList({ patient, context, removeState, confirmRemoval, ind
                 <div className="card-body text-center">
                     <h2 className="card-title">{name}</h2>
                     <p className="card-text"> 
-                        Access {name !== 'name this patient' ? name : 'this patient'}'s information here
+                        Access {name !== 'name this patient' ? name : 'this patient'}'s information here.
                     </p>
                 </div>
             </div>
