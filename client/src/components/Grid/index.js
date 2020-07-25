cd02d43275c4dd13bbf99ab689f82cf981e4a3d0
My Lord <page.c.tyler@gmail.com>
2020-07-24 20:11:17 -0400

    resolved issue where some of the grid components were assigning classes that were 'undefined'.

--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
-        
-export const Container = ({ classes, children }) => <div className={`container ${classes}`}>{children}</div>
 
-export const Row = ({ classes, children }) => <div className={`row ${classes}`}>{children}</div>
+export const Container = ({ classes, children }) => <div className={classes ? `container ${classes}`: 'container'}>{children}</div>
+
+export const Row = ({ classes, children }) => <div className={classes ? `row ${classes}`: 'row'}>{children}</div>
 
 export const Col = ({ size, classes, children }) => {
+    const theClass = classes ? classes : ''
     return ( <div 
         className={size.split(' ').map( size => `col-${size}`).join(' ')
-        +" "+ classes}>{children}</div>
+        +" "+ theClass}>{children}
+        </div>
     )
-}
+    }
 
 export const Collapse = ({ navState, children, id }) => { 
     const classes = navState ? 'navbar-collapse collapse show' : 'navbar-collapse collapse';
